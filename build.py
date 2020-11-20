982e3aaefa78b95f853c7b54388c6d1267f1b6e7
Lukas Masuch <lukas.masuch@gmail.com>
2020-11-20 04:21:08 +0100

    Add build script to project root

---
+++ build.py
@@ -0,0 +1,96 @@
+import os
+from typing import Dict, List, Union
+
+from universal_build import build_utils
+from universal_build.helpers import build_python
+
+# Project specific configuration
+MAIN_PACKAGE = "universal_build"
+GITHUB_URL = "https://github.com/ml-tooling/universal-build"
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def main(args: dict) -> None:
+    # set current path as working dir
+    os.chdir(HERE)
+
+    version = args[build_utils.FLAG_VERSION]
+
+    if version:
+        # Update version in _about.py
+        build_python.update_version(
+            os.path.join(HERE, f"src/{MAIN_PACKAGE}/_about.py"), str(version)
+        )
+
+    if args.get(build_utils.FLAG_MAKE):
+        # Install pipenv dev requirements
+        build_python.install_build_env()
+        # Create API documentation via lazydocs
+        build_python.generate_api_docs(github_url=GITHUB_URL, main_package=MAIN_PACKAGE)
+        # Build distribution via setuptools
+        build_python.build_distribution()
+
+    if args.get(build_utils.FLAG_CHECK):
+        build_python.code_checks(exit_on_error=True)
+
+    if args.get(build_utils.FLAG_TEST):
+        # Remove coverage files
+        build_utils.run("pipenv run coverage erase", exit_on_error=False)
+
+        test_markers = args.get(build_utils.FLAG_TEST_MARKER)
+
+        if (
+            isinstance(test_markers, list)
+            and build_utils.TEST_MARKER_SLOW in test_markers
+        ):
+            # Run if slow test marker is set: test in multiple environments
+            # Python 3.6
+            build_python.test_with_py_version(python_version="3.6.12")
+
+            # Python 3.7
+            build_python.test_with_py_version(python_version="3.7.9")
+
+            # Activated Python Environment (3.8)
+            build_python.install_build_env()
+            # Run pytest in pipenv environment
+            build_utils.run("pipenv run pytest", exit_on_error=True)
+
+            # Update pipfile.lock when all tests are successfull (lock environment)
+            build_utils.run("pipenv lock", exit_on_error=True)
+        else:
+            # Run fast tests
+            build_utils.run('pipenv run pytest -m "not slow"', exit_on_error=True)
+
+    if args.get(build_utils.FLAG_RELEASE):
+        # Publish distribution on pypi
+        build_python.publish_pypi_distribution(
+            pypi_token=args.get(build_python.FLAG_PYPI_TOKEN),
+            pypi_repository=args.get(build_python.FLAG_PYPI_REPOSITORY),
+        )
+
+        # TODO: Publish coverage report: if private repo set CODECOV_TOKEN="token" or use -t
+        # build_utils.run("curl -s https://codecov.io/bash | bash -s", exit_on_error=False)
+
+    # Build the build environment component
+    build_utils.build("build-environment", args)
+
+
+if __name__ == "__main__":
+    args = build_python.get_sanitized_arguments()
+
+    if args.get(build_utils.FLAG_RELEASE):
+        # Run main without release to see whether everthing can be built and all tests run through
+        # Run args without release to see whether everthing can be built and all tests run through
+        args = dict(args)
+        args[build_utils.FLAG_RELEASE] = False
+        main(args)
+        # Run main again without building and testing the components again
+        args = {
+            **args,
+            build_utils.FLAG_MAKE: False,
+            build_utils.FLAG_CHECK: False,
+            build_utils.FLAG_TEST: False,
+            build_utils.FLAG_FORCE: True,
+        }
+    main(args)
