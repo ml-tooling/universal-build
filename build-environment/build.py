9d83cfbae26bbc6c3f60f606e49d5636ddb9be0b
Lukas Masuch <lukas.masuch@gmail.com>
2020-11-23 14:30:56 +0100

    Use more descriptive variable naming

--- build-environment/build.py
+++ build-environment/build.py
@@ -3,7 +3,7 @@ import os
 from universal_build import build_utils
 from universal_build.helpers import build_docker
 
-COMPONENT_NAME = "build-environment"
+IMAGE_NAME = "build-environment"
 DOCKER_IMAGE_PREFIX = "mltooling"
 
 HERE = os.path.abspath(os.path.dirname(__file__))
@@ -13,22 +13,21 @@ def main(args: dict) -> None:
     # set current path as working dir
     os.chdir(HERE)
 
+    version = args.get(build_utils.FLAG_VERSION)
     docker_image_prefix = DOCKER_IMAGE_PREFIX
     if args.get(build_docker.FLAG_DOCKER_IMAGE_PREFIX):
         docker_image_prefix = args.get(build_docker.FLAG_DOCKER_IMAGE_PREFIX)  # type: ignore
 
     if args.get(build_utils.FLAG_MAKE):
-        build_docker.build_docker_image(
-            COMPONENT_NAME, args.get(build_utils.FLAG_VERSION), exit_on_error=True
-        )
+        build_docker.build_docker_image(IMAGE_NAME, version, exit_on_error=True)
 
     if args.get(build_utils.FLAG_CHECK):
         build_docker.lint_dockerfile()
 
     if args.get(build_utils.FLAG_RELEASE):
         build_docker.release_docker_image(
-            COMPONENT_NAME,
-            args.get(build_utils.FLAG_VERSION),
+            IMAGE_NAME,
+            version,
             docker_image_prefix,
             exit_on_error=True,
         )
