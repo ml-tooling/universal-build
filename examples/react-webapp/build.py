8d0e3742a0dd25db6575d0cc97c209c874b1fcdd
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2021-02-08 19:39:09 +0100

    Run prettier in webapp's check phase
    Besides bringing the code to the right format,
    it also prevents differences between existing and
    generated client code

--- examples/react-webapp/build.py
+++ examples/react-webapp/build.py
@@ -6,6 +6,9 @@ build_utils.log("Install essentials")
 build_utils.run("yarn install", exit_on_error=False)
 
 if args.get(build_utils.FLAG_CHECK):
+    build_utils.log("Run prettier:")
+    build_utils.run("yarn run prettier src/", exit_on_error=True)
+
     build_utils.log("Run linters:")
     build_utils.run("yarn run lint:js", exit_on_error=True)
     build_utils.run("yarn run lint:css", exit_on_error=True)
